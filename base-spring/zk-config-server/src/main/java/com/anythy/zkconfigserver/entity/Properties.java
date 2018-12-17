8163d128bb013848a516608c2e4231a33e768de3
孔令旗 <konglq@microseer.com>
2018-12-17 18:26:18 +0800

    测试类

---
+++ base-spring/zk-config-server/src/main/java/com/anythy/zkconfigserver/entity/Properties.java
@@ -0,0 +1,25 @@
+package com.anythy.zkconfigserver.entity;
+
+/**
+ * @author Leach
+ */
+public class Properties {
+    private String path;
+    private String data;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
--- base-spring/zk-config-server/src/main/java/com/anythy/zkconfigserver/util/ZookeeperUtil.java
+++ base-spring/zk-config-server/src/main/java/com/anythy/zkconfigserver/util/ZookeeperUtil.java
@@ -1,9 +1,10 @@
 package com.anythy.zkconfigserver.util;
 
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.ZooDefs;
-import org.apache.zookeeper.ZooKeeper;
+import com.anythy.base.common.utils.JsonHelper;
+import com.anythy.zkconfigserver.entity.Properties;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.apache.zookeeper.version.util.VerGen;
 
 import java.io.IOException;
 
@@ -13,11 +14,29 @@ import java.io.IOException;
 public class ZookeeperUtil {
     public static void main(String[] args) {
         try {
-            ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", 3000, (event) -> {
+            ZooKeeper zk = new ZooKeeper("140.143.236.179:2181", 30000, (event) -> {
                 System.out.println("connection success");
             });
-            zk.create("/test", "ttttt".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-            System.out.println(zk);
+//            zk.create("/test", "ttttt".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+            Properties properties = new Properties();
+            properties.setData("测试数据");
+            properties.setPath("ppppp");
+
+            zk.setData("/test", JsonHelper.toJSON(properties).getBytes(), Version.REVISION);
+            Stat stat = new Stat();
+            byte[] bytes = zk.getData("/test", true, stat);
+            Properties p = new Properties();
+            zk.getData("/test", true, (v1, v2, v3, v4, v5) -> {
+                System.out.println(v1);
+                System.out.println(v2);
+                System.out.println(v3);
+                System.out.println(v4);
+                System.out.println(v5);
+            }, p);
+            p = JsonHelper.fromJSON(new String(bytes), Properties.class);
+            System.out.println(p);
+            System.out.println(stat);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
